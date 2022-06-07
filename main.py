58d55b63e21d138ff8bf9b5d60eb88cb93882aff
nadirahaddach <89223568+nadirahaddach@users.noreply.github.com>
2022-06-06 20:27:23 -0700

    start

---
+++ main.py
@@ -0,0 +1,34 @@
+from __init__ import app
+from flask import Flask, render_template, request
+
+from cruddy.app_crud import app_crud
+from cruddy.app_crud_api import app_crud_api
+
+app.register_blueprint(app_crud)
+app.register_blueprint(app_crud_api)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/password')
+def password():
+    return render_template("password.html")
+
+
+@app.route('/guess')
+def guess():
+    return render_template("guess.html")
+
+@app.route('/findyour/', methods=['GET', 'POST'])
+def findyour():
+    if request.form:
+        input = request.form.get("lname")
+        print("works")
+        if len("input") != 0:
+            return render_template("findyour.html", input=input)
+    return render_template("findyour.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
